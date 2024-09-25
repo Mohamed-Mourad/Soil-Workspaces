51b2d5a780926c32a01674ad4d2a1193d905c07a
Mohamed Mourad <mohamed.m_2000@outlook.com>
2024-09-25 09:54:57 +0300

    recreated project

---
+++ soil_flutter/android/app/src/main/kotlin/com/example/soil_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.soil_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- soil_flutter/example/soil_flutter_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:soil_flutter/soil_flutter.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
