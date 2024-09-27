80c1dd439de9047034bfaeabafac730ae4978bb5
Mohamed Mourad <mohamed.m_2000@outlook.com>
2024-09-27 17:31:23 +0300

    update workspace model

--- soil_flutter/lib/models/workspace_model.dart
+++ soil_flutter/lib/models/workspace_model.dart
@@ -1,12 +1,14 @@
 import 'package:soil_flutter/models/amenities_model.dart';
 
 class Workspace {
+  int id;
   String name;
   List<String> images;
   String location;
   Amenities amenities;
 
   Workspace({
+    required this.id,
     required this.name,
     required this.images,
     required this.location,
@@ -15,6 +17,7 @@ class Workspace {
 
   factory Workspace.fromJson(Map<String, dynamic> json) {
     return Workspace(
+      id: json['id'],
       name: json['name'],
       images: List<String>.from(json['images']),
       location: json['location'],
