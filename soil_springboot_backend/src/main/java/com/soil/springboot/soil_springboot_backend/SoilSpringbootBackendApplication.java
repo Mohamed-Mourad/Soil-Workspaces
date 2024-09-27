5018161a4f3fb2f1a9b4fd00233f8a337723fceb
Mohamed Mourad <mohamed.m_2000@outlook.com>
2024-09-27 19:42:10 +0300

    created backend project

---
+++ soil_springboot_backend/src/main/java/com/soil/springboot/soil_springboot_backend/SoilSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soil.springboot.soil_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilSpringbootBackendApplication.class, args);
+	}
+
+}
