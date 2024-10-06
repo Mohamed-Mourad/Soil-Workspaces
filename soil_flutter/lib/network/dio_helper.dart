9fdc1808a7f365e56ec1ef9b510d4314ec62a061
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2024-10-06 06:23:41 +0300

    Update dio_helper.dart

--- soil_flutter/lib/network/dio_helper.dart
+++ soil_flutter/lib/network/dio_helper.dart
@@ -6,7 +6,7 @@ class DioHelper {
   static init(){
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://192.168.1.5:8080/api/',
+        baseUrl: 'http://{your IP address}:8080/api/',
         receiveDataWhenStatusError: true,
       ),
     );
